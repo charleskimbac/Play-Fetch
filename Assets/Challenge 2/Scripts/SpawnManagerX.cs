635b9cf17fc40a1a8d3195f51d42e3ffdb1b7560
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2023-01-11 00:10:31 -0500

    cam pov fix, ball random spawnrate changed (+public now)

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,12 +9,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -26;
     private float spawnLimitXRight = -1;
     private float spawnPosY = 25f;
-    /*
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
-    */
+    
     private int random;
     private WaitForSeconds spawnInterval;
+    Vector3 spawnPos;
+    public float randomMin = 1.5f;
+    public float randomMax = 2.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +26,12 @@ public class SpawnManagerX : MonoBehaviour
     IEnumerator SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
         random = Random.Range(0, 2 + 1);
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[random], spawnPos, ballPrefabs[random].transform.rotation);
 
-        spawnInterval = new WaitForSeconds(Random.Range(3f, 5f));
+        spawnInterval = new WaitForSeconds(Random.Range(randomMin, randomMax));
         yield return spawnInterval;
 
         StartCoroutine(SpawnRandomBall());
